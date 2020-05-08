c311133c888deba66bc2bb261e2fc69141daa34a
Hannah Sandusky <hnnahsandusky@gmail.com>
2020-05-08 02:06:36 +0000

    Automatically backed up by Learn

--- lib/while.rb
+++ lib/while.rb
@@ -4,10 +4,3 @@ def using_while
     puts "Wingardium Leviosa"
     levitation force += 1
 end
-
-def using_until
-  levitation_force = 6
-  until levitation_force == 10
-    puts "Wingardium Leviosa"
-    levitation_force += 1
-end
